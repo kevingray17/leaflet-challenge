4ca2b21b02139695481f03fc06bb507bdd97bdbd
KG17 <kevingray17@gmail.com>
2021-01-19 15:18:26 -0500

    create json layer containing the features array

--- logic.js
+++ logic.js
@@ -15,7 +15,19 @@ d3.json(queryUrl, function(data) {
     function onEachFeature(feature, layer) {
       layer.bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  
+    };    
+// Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
 
 
 
