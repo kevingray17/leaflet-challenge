680566ce215c09a909ca0ecfb100d80d79410897
KG17 <kevingray17@gmail.com>
2021-01-19 10:36:48 -0500

    set up the geo json request

--- logic.js
+++ logic.js
@@ -1,3 +1,8 @@
 // Set a query URL for All Earthquakes Over the Last 30 Days
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
  
+// Perform a get request to the query URL
+d3.json(queryURL, function(data) {
+    // Once we get a response, send the data.features object to the CreateFeatures function
+    createFeatures(data.features);
+});
