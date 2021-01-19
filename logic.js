225784f71c5de7e8652929b37516d23fe5f1566d
KG17 <kevingray17@gmail.com>
2021-01-19 15:12:16 -0500

    define function

--- logic.js
+++ logic.js
@@ -1,18 +1,21 @@
+
 // Set a query URL for All Earthquakes Over the Last 30 Days
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
- 
-// Perform a get request to the query URL
-d3.json(queryURL, function(data) {
-    // Once we get a response, send the data.features object to the CreateFeatures function
-    createFeatures(data.features);
-    console.log(data.features)
-});
-// Define the function we want to run once for each feature in the features array
-// Give each feature a pop up describing the magnitude and depth of the earthquakes
 
-function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-}
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+  });
+  
+  function createFeatures(earthquakeData) {
+  
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  
+
 
 
