4a2721506dc9637e58dee72d68fea3b262ac474f
KG17 <kevingray17@gmail.com>
2021-02-01 11:45:15 -0500

    changed to 24 hours

--- logic.js
+++ logic.js
@@ -51,7 +51,7 @@ else  {
 
 // make geojson connection and grab the data for a month
 
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
 d3.json(url, (response) => {
   var earthquakes = response.features;
